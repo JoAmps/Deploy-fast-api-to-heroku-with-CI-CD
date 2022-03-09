097f5be568274ba7d4e97ede10563f05d2fcad80
Hyacinth Ampadu <ampaduh@gmail.com>
2022-03-09 18:09:19 +0000

    Inital commit

---
+++ main.py
@@ -0,0 +1,118 @@
+# Put the code for your API here.
+from typing import Union 
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import sys
+#sys.path.insert(0, '/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project 3/starter/starter')
+#sys.path.insert(0,'/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project 3/starter/starter/ml')
+from model import model_functions
+from model import data
+import pandas as pd
+import pickle
+from joblib import load
+from typing import Literal
+import numpy as np
+from pandas.core.frame import DataFrame
+
+cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+
+class User(BaseModel):
+    age: int
+    workclass: Literal[
+        'State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov',
+        'Local-gov', 'Self-emp-inc', 'Without-pay']   
+    education: Literal[
+        'Bachelors', 'HS-grad', '11th', 'Masters', '9th',
+        'Some-college',
+        'Assoc-acdm', '7th-8th', 'Doctorate', 'Assoc-voc', 'Prof-school',
+        '5th-6th', '10th', 'Preschool', '12th', '1st-4th']   
+    maritalStatus: Literal[
+        'Never-married', 'Married-civ-spouse', 'Divorced',
+        'Married-spouse-absent', 'Separated', 'Married-AF-spouse',
+        'Widowed']
+    occupation: Literal[
+        'Adm-clerical', 'Exec-managerial', 'Handlers-cleaners',
+        'Prof-specialty', 'Other-service', 'Sales', 'Transport-moving',
+        'Farming-fishing', 'Machine-op-inspct', 'Tech-support',
+        'Craft-repair', 'Protective-serv', 'Armed-Forces',
+        'Priv-house-serv']
+    relationship: Literal[
+        'Not-in-family', 'Husband', 'Wife', 'Own-child',
+        'Unmarried', 'Other-relative']
+    race: Literal[
+        'White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo',
+        'Other']
+    sex: Literal['Male', 'Female']
+    hoursPerWeek: int
+    nativeCountry: Literal[
+        'United-States', 'Cuba', 'Jamaica', 'India', 'Mexico',
+        'Puerto-Rico', 'Honduras', 'England', 'Canada', 'Germany', 'Iran',
+        'Philippines', 'Poland', 'Columbia', 'Cambodia', 'Thailand',
+        'Ecuador', 'Laos', 'Taiwan', 'Haiti', 'Portugal',
+        'Dominican-Republic', 'El-Salvador', 'France', 'Guatemala',
+        'Italy', 'China', 'South', 'Japan', 'Yugoslavia', 'Peru',
+        'Outlying-US(Guam-USVI-etc)', 'Scotland', 'Trinadad&Tobago',
+        'Greece', 'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 'Hungary',
+        'Holand-Netherlands']
+
+
+# Loading in model from serialized .pkl file
+with open('/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project 3/starter/starter/model.pkl', 'rb') as file:
+  model_object= pickle.load(file)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_items():
+    return {"message": "Hello, welcome to our app!"}
+
+@app.post("/")
+async def inferences(user_data: User):
+    #model=load("model.joblib")
+    encoder = load("/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project 3/starter/starter/encoder.joblib")
+    lb = load("/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project 3/starter/starter/lb.joblib")    
+    array = np.array([[
+                     user_data.age,
+                     user_data.workclass,
+                     user_data.education, 
+                     user_data.maritalStatus,
+                     user_data.occupation,
+                     user_data.relationship,
+                     user_data.race,
+                     user_data.sex,
+                     user_data.hoursPerWeek,
+                     user_data.nativeCountry
+                     ]])
+
+    df_temp = DataFrame(data=array, columns=[
+        "age",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "hours-per-week",
+        "native-country",
+    ])                 
+    X, _, _, _ = data.process_data(
+                df_temp,
+                categorical_features=cat_features,
+                encoder=encoder, lb=lb, training=False)
+    pred = model_functions.inference(model_object,X)
+    y = lb.inverse_transform(pred)[0]
+    return {"prediction":y}
+
---
+++ model/train_model.py
@@ -0,0 +1,122 @@
+# Script to train machine learning model.
+
+from sklearn.model_selection import train_test_split
+import sys
+import logging
+import pickle
+from data import process_data
+from clean_data import load_data, cleaned_data
+from model_functions import train_model,compute_model_metrics,model_predictions
+from joblib import dump,load
+import os
+
+
+
+logging.basicConfig(
+    filename='./log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
+
+def test_import(cleaned_data):
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+        logging.info('SUCCESS!: Data has rows and columns!')
+    except AssertionError as err:
+        logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+        raise err    
+
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+
+def split_data(data):
+    try:
+        train, test = train_test_split(data, test_size=0.20, random_state=0)
+        logging.info('SUCCESS!:Data split successfully')
+        return train, test
+    except:
+        logging.info('Error!:Error whiles splitting data')          
+
+
+cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+   
+def test_model(train_model):
+    try:
+        if not X_train.shape[0] == y_train.shape[0]:
+            raise AssertionError(
+                (X_train.shape[0],
+                 y_train.shape[0]))
+        if not X_test.shape[0] == y_test.shape[0]:
+            raise AssertionError(
+                (X_test.shape[0],
+                 y_test.shape[0]))
+        logging.info('SUCCESS: dependent and independent training and testing data have equal lengths')         
+    except AssertionError as err:
+        logging.error(
+            "Lengths of the Independent and dependent training and testing data mismatch")
+        raise err      
+
+def test_metrics(compute_model_metrics):
+    try:
+        if not y_test.shape[0] == predictions.shape[0]:
+            raise AssertionError(
+                (y_test.shape[0],
+                predictions.shape[0]))
+                        
+        logging.info('SUCCESS: predictions and output test data have equal lengths')         
+    except AssertionError as err:
+        logging.error(
+            "Lengths of predictions and output test data mismatch")
+        raise err
+
+def model_slicing(data):
+    """
+    Slice model for categorical features
+    """
+    slice_values = []
+
+    for cat in cat_features:
+        for cls in test[cat].unique():
+            df_temp = test[test[cat] == cls]
+            X_test_temp, y_test_temp, _, _ =process_data(df_temp,categorical_features=cat_features,label="salary", encoder=encoder, lb=lb, training=False)
+            y_preds = model.predict(X_test_temp)
+            precision_temp, recall_temp, fbeta_temp = compute_model_metrics(y_test_temp, y_preds)
+            results = "[%s->%s] Precision: %s " \
+                   "Recall: %s FBeta: %s" % (cat, cls, precision_temp, recall_temp, fbeta_temp)
+            slice_values.append(results)
+
+    with open('slice_model_output.txt', 'w') as out:
+        for slice_value in slice_values:
+            out.write(slice_value + '\n')
+
+
+
+
+   
+
+if __name__ == '__main__':
+    df=load_data('/Users/hyacinthampadu/Documents/Jos Folder/Data Science/Udacity mL devops engineer/project_3_rearrangements/project 3/deploy_fastapi_heroku/data/census_cleaned.csv')
+    test_import(cleaned_data)
+    train,test=split_data(df)
+    X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_features, label="salary", training=True)
+    X_test, y_test, encoder_t, lb_t = process_data(test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
+    dump(encoder_t, "encoder.joblib")
+    dump(lb_t, "lb.joblib")
+    test_model(train_model)
+    model=train_model(X_train, y_train)
+    predictions=model_predictions(X_test, model)
+    test_metrics(compute_model_metrics)
+    precision, recall, fbeta=compute_model_metrics(y_test, predictions)
+    model_slicing(df)
+    
+
